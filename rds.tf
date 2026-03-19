c19b36fb214aadb74fa28d440531c734d2363c23
yelmame <161223292+yelmame@users.noreply.github.com>
2026-03-19 11:33:42 +0530

    Add AWS RDS MySQL instance configuration

---
+++ rds.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
