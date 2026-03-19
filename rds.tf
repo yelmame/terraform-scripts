d18984e6ad5a66ae4c535e8aac1c68288146b610
yelmame <rachanayelmame8.gmail.com>
2026-03-19 11:39:06 +0530

    password file is uploded

--- rds.tf
+++ rds.tf
@@ -8,7 +8,7 @@ resource "aws_db_instance" "default" {
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
   username             = "foo"
-  password             = "foobarbaz"
+  password             = file("./password.txt")
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 }
